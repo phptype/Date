1d939320a377b6a8bfb81e538cb56f9196399014
Bilge <bilge@scriptfusion.com>
2025-09-17 21:30:04 +0100

    Added optional format specifier for adaptive dates.

--- src/Date.php
+++ src/Date.php
@@ -13,20 +13,21 @@ final class Date
      * Adaptively transforms the specified date, producing a relative or absolute date depending upon how recent it is.
      *
      * @param string|\DateTimeInterface $date Date.
+     * @param string $format Optional. The format to use when the number of days is greater than 30.
      *
      * @return string Absolute or relative date.
      */
-    public static function adapt(string|\DateTimeInterface $date): string
+    public static function adapt(string|\DateTimeInterface $date, string $format = 'M Y'): string
     {
         $date = is_string($date) ? new \DateTimeImmutable($date) : $date;
         $diff = (new \DateTime)->diff($date);
-        // If diff less than one whole second from next day, round up to whole day.
+        // If diff less than one whole second from the next day, round up to a whole day.
         $absDays = ($diff->days + (int)(($diff->h * 60 ** 2 + $diff->i * 60 + $diff->s + ceil($diff->f)) / 86400));
         $days = $absDays * ($diff->invert ? -1 : 1);
 
         // Absolute.
         if ($absDays > 30) {
-            return $date->format('M Y');
+            return $date->format($format);
         }
 
         // Relative.
